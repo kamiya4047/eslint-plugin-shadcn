7aa8e2ef4e9fdb3ce20fa938e6082fc1297f2216
Kamiya <kamiya@kamiya.app>
2026-03-06 21:39:10 +0800

    fix: optimize build artifacts

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,41 @@
 import json from '@rollup/plugin-json';
 import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    file: 'dist/index.js',
-    format: 'es',
-    sourcemap: true,
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.js',
+      format: 'es',
+      sourcemap: true,
+    },
+    external: [
+      '@typescript-eslint/types',
+      'eslint',
+      'node:path',
+      'node:fs',
+    ],
+    plugins: [
+      json(),
+      typescript({
+        tsconfig: './tsconfig.json',
+        declaration: true,
+        declarationDir: 'dist/temp',
+        include: ['src/**/*'],
+      }),
+    ],
   },
-  external: [
-    '@typescript-eslint/types',
-    'eslint',
-    'node:path',
-    'node:fs',
-  ],
-  plugins: [
-    json(),
-    typescript({
-      tsconfig: './tsconfig.json',
-      declaration: true,
-      declarationDir: 'dist',
-      include: ['src/**/*'],
-    }),
-  ],
-};
+  {
+    input: 'dist/temp/index.d.ts',
+    output: {
+      file: 'dist/index.d.ts',
+      format: 'es',
+    },
+    external: [
+      '@typescript-eslint/types',
+      'eslint',
+    ],
+    plugins: [dts()],
+  },
+];
